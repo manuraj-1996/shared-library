fc9e123aa7a63fec36c24b1cd9af4eb44bbfe4fb
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-28 19:49:09 +0530

    Refactor ecrLogin.groovy to use pipeline stages

--- vars/ecrLogin.groovy
+++ vars/ecrLogin.groovy
@@ -1,16 +1,12 @@
-def call (Map config) {
-def credentialsId = config.get('credentialsId')
-def region = config.get('region')
-def accountId = config.get('accountId')
-def ecrCredentialsId = config.get('ecrCredentialsId')
-if (!credentialsId || !region || !accountId) { 
-error "credentialsId, region and accountId are required"
-}
-withCredentials ([[ $class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-creds' ]]) {
-sh """
-set -e
-aws sts get-caller-identity
-aws ecr get-login-password --region ${region} | \docker login --username AWS --password-stdin \ ${accountId}.dkr.ecr.${region}.amazonaws.com
-"""
-}
-}
+stage('Initialize') {
+            steps {
+                script {
+                    def config = constant(params.AWS)
+                    env.ACCOUNT_ID = config.ACCOUNT_ID
+                    env.AWS_REGION = config.AWS_REGION
+                    env.ECR_REPO   = config.ECR_REPO
+                    env.ECR_URL    = config.ECR_URL
+                    env.GIT_URL    = config.GIT_URL
+                }
+            }
+        }
