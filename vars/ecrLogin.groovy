e3dca564b0b5a25d850573d45235e3fcdab99dcb
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-28 19:32:49 +0530

    Update ecrLogin.groovy

--- vars/ecrLogin.groovy
+++ vars/ecrLogin.groovy
@@ -3,13 +3,14 @@ def credentialsId= config.get('credentialsId')
 def region= config.get('region')
 def accountId= config.get('accountId')
 def ecrCredentialsId= config.get('ecrCredentialsId')
-if(!credentialsId || !region || !accountId) { 
+if (!credentialsId || !region || !accountId) { 
 error "credentialsId, region and accountId are required"
 }
-withcredentials ([[ $class: 'AmazonWebServicesCredentialsBinding', credentialsId: credentialsId ]]) {
+withCredentials ([[ $class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'aws-creds' ]]) {
 sh """
+set -e
 aws sts get-caller-identity
-aws ecr get-loginpassword--region${region}|\docker login--username AWS\--password-stdin ${accountId}.dkr.ecr.${region}.amazonaws.com
+aws ecr get-login-password --region ${region} | \docker login --username AWS --password-stdin \ ${accountId}.dkr.ecr.${region}.amazonaws.com
 """
 }
 }
