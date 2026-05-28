94c763787d614eea47dc67b7de62b5601e2f7deb
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-28 17:25:15 +0530

    updated

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,9 +1,9 @@
-def Call(MapConfig= [:]){
-def imageName= config.get('imageName')
-if(!imageName){
+def call(Map Config = [:]) {
+def imageName = config.get('imageName')
+if (!imageName) {
 error "imageName is required"
 }
-sh"""
-docker build -t ${imageName}
+sh """
+docker build -t ${imageName} .
 """
 }
