9c49a0c07772d6d1cce13e8f0f9492578dba4192
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-28 19:47:35 +0530

    New

--- vars/awsEnv.groovy
+++ vars/awsEnv.groovy
@@ -1,7 +1,17 @@
-def call(Map config) {
-script.env.AWS_REGION= 'us-east-1'
-script.env.ACCOUNT_ID= '772064137213'
-script.env.ECR_REPO= 'ecommerce'
-script.env.ECR_URL= '772064137213.dkr.ecr.us-east-1.amazonaws.com/ecommerce'
-script.env.IMAGE_NAME= 'ecommerce'
+def call(AWS) {
+
+    def ACCOUNT_ID = '772064137213'
+    def AWS_REGION = 'us-east-1'
+    def ECR_REPO   = 'ecommerce'
+    def GIT_URL    = "https://github.com/manuraj-1996/whatsapp.git"
+    def ECR_URL = "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}"
+ def IMAGE_NAME = AWS
+    return [
+        ACCOUNT_ID: ACCOUNT_ID,
+        AWS_REGION: AWS_REGION,
+        ECR_REPO  : ECR_REPO,
+        ECR_URL   : ECR_URL,
+        GIT_URL   : GIT_URL,
+        IMAGE_NAME : IMAGE_NAME
+    ]
 }
