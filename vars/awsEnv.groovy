5f2a69bebd5aa885f69ce183a78296f96f842eca
Manoj <manojgopalraj1996@gmail.com>
2026-05-28 17:16:43 +0530

    update

--- vars/awsEnv.groovy
+++ vars/awsEnv.groovy
@@ -1,7 +1,7 @@
 def Call(script){
 script.env.AWS_REGION= 'us-east-1'
 script.env.ACCOUNT_ID= '772064137213'
-script.env.ECR_REPO= 'devopsproject'
-script.env.ECR_URL= '772064137213.dkr.ecr.us-east-1.amazonaws.com/devopsproject'
-script.env.IMAGE_NAME= 'devopsproject'
+script.env.ECR_REPO= 'ecommerce'
+script.env.ECR_URL= '772064137213.dkr.ecr.us-east-1.amazonaws.com/ecommerce'
+script.env.IMAGE_NAME= 'ecommerce'
 }
