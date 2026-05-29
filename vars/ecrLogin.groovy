de49ee0298d1de3680675340f1ed0d6b6ff35950
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-29 10:01:16 +0530

    Update ecrLogin.groovy

--- vars/ecrLogin.groovy
+++ vars/ecrLogin.groovy
@@ -1,12 +1,7 @@
-stage('Initialize') {
-            steps {
-                script {
-                    def config = constant(params.AWS)
-                    env.ACCOUNT_ID = config.ACCOUNT_ID
-                    env.AWS_REGION = config.AWS_REGION
-                    env.ECR_REPO   = config.ECR_REPO
-                    env.ECR_URL    = config.ECR_URL
-                    env.GIT_URL    = config.GIT_URL
-                }
-            }
-        }
+def call() {
+
+    sh """
+        aws ecr get-login-password --region ${env.AWS_REGION} | \
+        docker login --username AWS --password-stdin ${env.ECR_URL}
+    """
+}
