7ceffccfd09311376378bcc392b5f405052f2afa
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-29 09:57:58 +0530

    Update awsEnv.groovy

--- vars/awsEnv.groovy
+++ vars/awsEnv.groovy
@@ -1,7 +1,19 @@
-def call(script, Map config) {
+def call(String envName) {
 
-    script.env.AWS_REGION = config.region
-    script.env.ACCOUNT_ID = config.accountId
-    script.env.ECR_REPO   = config.repo ?: 'ecommerce'
-    script.env.ECR_URL    = "${config.accountId}.dkr.ecr.${config.region}.amazonaws.com/${script.env.ECR_REPO}"
+    def config = [:]
+
+    if (envName == 'ecommerce') {
+
+        config.ACCOUNT_ID = '772064137213'
+        config.AWS_REGION = 'us-east-1'
+        config.ECR_REPO   = 'ecommerce-app'
+        config.ECR_URL    = "${config.ACCOUNT_ID}.dkr.ecr.${config.AWS_REGION}.amazonaws.com/${config.ECR_REPO}"
+        config.GIT_URL    = 'https://github.com/manuraj-1996/whatsapp.git'
+
+    } else {
+
+        error "Unknown environment: ${envName}"
+    }
+
+    return config
 }
