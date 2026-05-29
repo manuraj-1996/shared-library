13e83f2ad4fe9b9db892643f8db219afa6776af8
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-29 10:01:59 +0530

    Update dockerTag.groovy

--- vars/dockerTag.groovy
+++ vars/dockerTag.groovy
@@ -1,12 +1,7 @@
-def call (Map config = [:]) {
-def imageName= config.get('imageName')
-def ecrUrl= config.get ('ecrUrl')
-def tag= config.get('tag','latest')
-if(!imageName || !ecrUrl) {
-error "imagename and ecrUrl are required"
-}
-echo "Tagging ${imageName}- ${ecrUrl}:${tag}"
-sh"""
-dockertag ${imageName}${ecrUrl:}:${tag}
-"""
+def call(Map config) {
+
+    sh """
+        docker tag ${config.imageName}:${config.tags} \
+        ${config.ecrURL}:${config.tags}
+    """
 }
