0b206ad89515028f75c9d0558850a7be24ebeb9d
manuraj-1996 <manojgopalraj1996@gmail.com>
2026-05-30 15:07:41 +0530

    Update ecrLogin.groovy

--- vars/ecrLogin.groovy
+++ vars/ecrLogin.groovy
@@ -1,12 +1,14 @@
-withCredentials([
-    [$class: 'AmazonWebServicesCredentialsBinding',
-     credentialsId: 'aws-ecr-creds']
-]) {
+def call() {
+    withCredentials([
+        [$class: 'AmazonWebServicesCredentialsBinding',
+         credentialsId: 'aws-ecr-creds']
+    ]) {
 
-    sh '''
-    aws ecr get-login-password --region us-east-1 | \
-    docker login \
-      --username AWS \
-      --password-stdin 772064137213.dkr.ecr.us-east-1.amazonaws.com
-    '''
+        sh '''
+        aws ecr get-login-password --region us-east-1 | \
+        docker login \
+          --username AWS \
+          --password-stdin 772064137213.dkr.ecr.us-east-1.amazonaws.com
+        '''
+    }
 }
